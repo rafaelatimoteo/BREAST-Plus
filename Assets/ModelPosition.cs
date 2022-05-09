ce6f07d6dc90e4dbe91bd90677d0885b4d5c2d5d
Rafaela Timóteo <rafaelatimoteo@live.com.pt>
2022-05-09 12:25:02 +0100

    Arrow buttons for position precision working

--- Assets/ModelPosition.cs
+++ Assets/ModelPosition.cs
@@ -19,13 +19,38 @@ public class ModelPosition : MonoBehaviour
     // Update is called once per frame
     public void moveLeft()
     {
-        model.transform.position = model.transform.position + new Vector3(-0.1f, 0, 0);
+        model.transform.position = model.transform.position + new Vector3(-0.001f, 0, 0);
           
     }
 
     public void moveRight()
     {
-        model.transform.position = model.transform.position + new Vector3(0.1f, 0, 0);
+        model.transform.position = model.transform.position + new Vector3(0.001f, 0, 0);
 
     }
+
+    public void moveUp()
+    {
+        model.transform.position = model.transform.position + new Vector3(0, -0.001f, 0);
+
+    }
+
+    public void moveDown()
+    {
+        model.transform.position = model.transform.position + new Vector3(0, 0.001f, 0);
+
+    }
+
+    public void moveBack()
+    {
+        model.transform.position = model.transform.position + new Vector3(0, 0, -0.001f);
+
+    }
+
+    public void moveForward()
+    {
+        model.transform.position = model.transform.position + new Vector3(0, 0, 0.001f);
+
+    }
+
 }
